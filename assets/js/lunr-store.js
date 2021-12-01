6f85a20eba818c7c374f4b71e8d0ed39804cbfba
evanwill <evanwill@users.noreply.github.com>
2021-12-01 20:13:44 +0000

    deploy: 852f04f741df42245d43e1696fc68f7ff128ddc3

---
+++ assets/js/lunr-store.js
@@ -0,0 +1,97 @@
+var store = [{  
+"title": "Morant Bay Rebellion 1","date": "1865-11-10", 
+"id": "mbr_1"
+
+},{  
+"title": "Morant Bay Rebellion 2","date": "1865-11-11", 
+"id": "mbr_2"
+
+},{  
+"title": "Morant Bay Rebellion 3","date": "1865-11-13", 
+"id": "mbr_3"
+
+},{  
+"title": "Morant Bay Rebellion 4","date": "1865-11-15", 
+"id": "mbr_4"
+
+},{  
+"title": "Morant Bay Rebellion 5","date": "1865-11-17", 
+"id": "mbr_5"
+
+},{  
+"title": "Morant Bay Rebellion 6","date": "1865-11-17", 
+"id": "mbr_6"
+
+},{  
+"title": "Morant Bay Rebellion 7","date": "1865-11-17", 
+"id": "mbr_7"
+
+},{  
+"title": "Morant Bay Rebellion 8","date": "1865-11-18", 
+"id": "mbr_8"
+
+},{  
+"title": "Morant Bay Rebellion 9","date": "1865-11-21", 
+"id": "mbr_9"
+
+},{  
+"title": "Morant Bay Rebellion 10","date": "1865-11-24", 
+"id": "mbr_10"
+
+},{  
+"title": "Morant Bay Rebellion 11","date": "1865-12-19", 
+"id": "mbr_11"
+
+},{  
+"title": "Morant Bay Rebellion 12","date": "1866-01-04", 
+"id": "mbr_12"
+
+},{  
+"title": "Morant Bay Rebellion 13","date": "1866-01-19", 
+"id": "mbr_13"
+
+},{  
+"title": "Morant Bay Rebellion 14","date": "1866-04-27", 
+"id": "mbr_14"
+
+},{  
+"title": "Morant Bay Rebellion 15","date": "1866-05-21", 
+"id": "mbr_15"
+
+},{  
+"title": "Morant Bay Rebellion 16","date": "1868-05-23", 
+"id": "mbr_16"
+
+},{  
+"title": "Morant Bay Rebellion 17","date": "1865-11-11", 
+"id": "mbr_17"
+
+},{  
+"title": "Morant Bay Rebellion 18","date": "1865-11-22", 
+"id": "mbr_18"
+
+},{  
+"title": "Morant Bay Rebellion 19","date": "1865-11-24", 
+"id": "mbr_19"
+
+},{  
+"title": "Morant Bay Rebellion 20","date": "1865-12-15", 
+"id": "mbr_20"
+
+},{  
+"title": "Morant Bay Rebellion 21","date": "1865-12-16", 
+"id": "mbr_21"
+
+},{  
+"title": "Morant Bay Rebellion 22","date": "1865-12-21", 
+"id": "mbr_22"
+
+},{  
+"title": "Morant Bay Rebellion 23","date": "1865-12-29", 
+"id": "mbr_23"
+
+},{  
+"title": "Morant Bay Rebellion 24","date": "1866-08-02", 
+"id": "mbr_24"
+
+}];
